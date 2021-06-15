232373785f3ee39c3740f58cf8020ea75502c99a
Amanda <mandathompson@yahoo.com>
2021-06-15 15:21:31 -0400

    added two new pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>Bio goes here</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Info </h1>
+            <p>Contact Info Here</p>
+        </div>
+    )
+}
+
+export default Contact
